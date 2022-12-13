078407bc18be603d5758ccbd9ed60c02fd4fbed8
b-kubick <119148770+b-kubick@users.noreply.github.com>
2022-12-12 19:48:42 -0500

    Updated database files
    dataformatting.py formates the original .csv file keeping only the information we need
    database_creation.py creates and establishes the connection to movie_rec database. Furthermore it creates a table and inserts the information from the csv file to the newly created DB
    tmdbs_5000_movies.csv is the original .csv file
    dataset.csv file is the formatted file

---
+++ database_creation.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import csv
+
+import mysql.connector as mysql
+from mysql.connector import Error
+
+
+moviedata = pd.read_csv('/Users/besjana/PyCharm/dataset/dataset.csv', index_col=False, delimiter=',', quotechar='"',quoting=csv.QUOTE_ALL)
+
+# creates connection to the machine and creates there a Database called movie_rec
+try:
+    mydb = mysql.connect(
+        host="localhost",
+        username="root",
+        password="",
+        
+    )
+    if mydb.is_connected():
+        create_db_query = "CREATE DATABASE movie_rec"
+        with mydb.cursor() as mycursor:
+            mycursor.execute(create_db_query)
+except Error as e:
+    print("Error while creating DB", e)
+
+# connects to the movie_rec db created above and creates a new table
+try:
+    mydb = mysql.connect(
+        host="localhost",
+        username="root",
+        password="",
+        database="movie_rec"
+    )
+    if mydb.is_connected():
+        mycursor = mydb.cursor()  
+        mycursor.execute("select database();")
+        record = mycursor.fetchone()
+        print("You are connected to database: ", record)
+        mycursor.execute('DROP TABLE IF EXISTS movies;')
+        print('Creating table.....')
+
+        mycursor.execute("CREATE TABLE movies(id INTEGER PRIMARY KEY NOT NULL,"
+                         "genres TEXT,"
+                         "plot TEXT,"
+                         "release_date DATE NOT NULL,"
+                         "ratings FLOAT NOT NULL,"
+                         "title TEXT NOT NULL)")
+
+        print('Table is created....')
+
+        #  loop through the data frame
+        for i, row in moviedata.fillna(-1).iterrows():
+            sql = "INSERT INTO movie_rec.movies VALUES (%s,%s,%s,%s,%s,%s)"
+            mycursor.execute(sql, tuple(row))
+            print("Record inserted")
+            # the connection is not auto committed by default, so we must commit to save the changes
+            mydb.commit()
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+
+# closing the database connection
+# connection.close()
